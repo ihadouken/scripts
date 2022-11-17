b613365c554467870bead45e11aa53c1db982554
Rahul Sharma <rsharma842756@gmail.com>
2022-11-17 13:05:08 +0530

    removed hard coded socket from mpvctl

--- mpvctl.sh
+++ mpvctl.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-socket=${XDG_CACHE_HOME:="$HOME/.cache"}/mpv-ipc-socket
+if [[ "$1" != '-s' ]]; then
+    echo "Specify a socket with '-s' as the 1st argument"
+    exit
+fi
 
-while getopts "bfpr" arg; do
+# if [[ "$*" != *"s"* ]]; then
+#     echo "Specify a socket with '-s' as the 1st argument"
+#     exit
+# fi
+
+while getopts "s:bfpr" arg; do
     case "$arg" in
+        s)  socket="$OPTARG" ;;
         b)  echo '{ "command": [ "seek", "-10" ] }' | socat - "$socket" > /dev/null 2>&1 ;;
-        f)  echo '{ "command": [ "seek", "+10" ] }' | socat - "$socket" > /dev/null 2>&1 ;;
+        f)  echo "$socket" && echo '{ "command": [ "seek", "+10" ] }' | socat - "$socket" > /dev/null 2>&1 ;;
         p)  echo 'cycle pause' | socat - "$socket" ;;
         r)  echo '{ "command": [ "set_property", "time-pos", "0" ] }' | socat - "$socket" > /dev/null 2>&1 ;;
         *)  echo "Invalid option !!"
