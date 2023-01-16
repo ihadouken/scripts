59865ecadcbae62791faf35b333a8e06da5cbd23
Rahul Sharma <rsharma842756@gmail.com>
2023-01-16 22:15:56 +0530

    Use meaningful default if XDG variables unset.
    In previous commits, scripts exited with an error message when some
    required XDG variable was not set.

--- alert.sh
+++ alert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-paplay ${XDG_DATA_HOME:="$HOME/.local/share"}/sounds/hey_listen.ogg
+paplay ${XDG_DATA_HOME:-"$HOME/.local/share"}/sounds/hey_listen.ogg
 # paplay /usr/share/sounds/freedesktop/stereo/message.oga
