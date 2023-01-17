e006dcf41e6c12b4c17ce2ce613c4efaaa115b56
Rahul Sharma <rsharma842756@gmail.com>
2023-01-17 06:37:17 +0530

    Async backup of dotfiles and app data

--- confbak.sh
+++ confbak.sh
@@ -1,90 +1,76 @@
 #!/bin/bash
-[[ -z $XDG_CONFIG_HOME || -z $XDG_DATA_HOME ]] && echo 'XDG_CONFIG_HOME or XDG_DATA_HOME not set, exiting ...' && exit 1
-
-### Some Definitions for use later. ###
-syncer="rsync --ignore-existing -avz"
-rootbak_dir=~/Backups
+syncer='rsync --ignore-existing -avz'
+rootbak_dir="$HOME/Backups"
 confbak_dir=$rootbak_dir/dots/config-backup-$(date +%Y.%m.%d-%H.%M.%S) 
 
 err() {
-    if [[ -z "$1" ]]; then
-        msg="Some error occured."
-    else
-        msg="$1"
-    fi
-    printf "\033[1;31m%s\033[0m\n" "$msg"
+    [[ -z "$1" ]] && msg='Some error occured.' || msg="$1"
+    printf '\033[1;31m%s\033[0m\n' "$msg"
     exit 1
 }
 
+data_dir_size="$(printf '%s' "$(du -sh "${XDG_DATA_HOME:-"$HOME/.local/share"}")")"
 
-### Some logging ... ###
-bat_logger.sh >> /var/hk_bat.log 2>/dev/null
-pacman -Qq > $XDG_CONFIG_HOME/installed_pkgs.log
-/usr/bin/ls -la > $XDG_CONFIG_HOME/home_contents.log
-
-data_dir_size=$(printf $(du -sh "$XDG_DATA_HOME"))
+### Dotfiles ###
+printf '\033[1;36m%s\033[0m\n' 'Backing dots ...'
+{ mkdir -p "$confbak_dir" && cp -Rf "${XDG_CONFIG_HOME:-"$HOME/.config"}" ~/.xmonad ~/.bash* ~/.pvpn-cli ~/.local/bin ~/.links ~/.Xresources ~/.telegram-cli ~/.mbsyncrc ~/.api_keys ~/.gnupg ~/.ssh ~/.password-store ~/.ngrok2 "$confbak_dir"; } &
+dotbak_proc=$!
 
 ### App Data ###
-printf "\033[1;34m%s\033[0m" "Do you want to sync your App-Data (about $data_dir_size) ? (y/n): "
-read -n 1 isSyncing
-
-if [[ $isSyncing == "y" ]]; then    
-    $syncer $XDG_DATA_HOME $rootbak_dir/Appdata --exclude='$XDG_DATA_HOME/warzone2100/sequences.wz' > /dev/null 2>&1 && printf "\nApp-Data synced.\n\n" || err "Failed to backup App-Data !!"
+read -n 1 bak_appdata -p "Do you want to backup your App-Data (about $data_dir_size) ? (y/n): "
+if [[ $bak_appdata == 'y' ]]; then
+    $syncer "${XDG_DATA_HOME:-"$HOME/.local/share"}" "$rootbak_dir"/Appdata &> /dev/null &
+    databak_proc=$!
 else
-    printf "\n\033[1;31m%s\033[0m\n\n" "[Warning]: App-Data won't be synced !"
+    printf '\n\033[1;31m%s\033[0m\n\n' "[Warning]: App data won't be synced !"
 fi
 
 
-### Dotfiles ###
-printf "\033[1;36m%s\033[0m\n" "Backing dots ..."
-mkdir -p $confbak_dir && cp -Rf $XDG_CONFIG_HOME ~/.xmonad ~/.bash* ~/.pvpn-cli ~/.local/bin ~/.links ~/.Xresources ~/.telegram-cli ~/.mbsyncrc ~/.api_keys ~/.gnupg ~/.ssh ~/.password-store ~/.ngrok2 $confbak_dir && \
-printf "\033[1;32m%s\033[0m\n\n" "Copied dots to Backups." || err "Couldn't backup dotfiles :\ "
+if ps -p $dotbak_proc; then
+    echo 'Waiting for dotfile backup to finish ...'
+    wait $dotbak_proc
+    [[ $? -eq 1 ]] && printf '\033[1;32m%s\033[0m\n\n' "Copied dots to $confbak_dir." \
+        || err "Couldn't backup dotfiles :\ "
+fi
 
-
-printf "\033[1;31m%s\033[0m\n" "[Warning]: This deletes all dotfile backups except the recent three."
-printf "\033[1;34m%s\033[0m" "Do you want to delete the old Backups ? (y/n): "
-read -n 1 isDeleting
-printf "\n"
-
-if [[ $isDeleting == "y" ]]; then
-    to_del=$(/bin/ls $rootbak_dir/dots/ | grep config | sort -n | head -n -3)
+printf '\033[1;31m%s\033[0m\n' '[Warning]: This deletes all dotfile backups except the recent three.'
+read -n 1 delete_old -p 'Do you want to delete the old Backups ? (y/n): '
+if [[ $delete_old == 'y' ]]; then
+    to_del=$(/bin/ls "$rootbak_dir/dots/" | grep config | sort -n | head -n -3)
     if [[ -n $to_del ]]; then
         for i in $to_del
         do
-            rm -rf $rootbak_dir/dotfiles/$i
+            rm -rf "$rootbak_dir"/dotfiles/"$i"
         done
     fi
 else
-    echo "Backups are preserved."
+    echo 'No backups removed.'
+fi
+
+if ps -p $databak_proc; then
+    echo 'Waiting for app data backup to finish ...'
+    wait $databak_proc
+    [[ $? -eq 1 ]] && printf '\033[1;32m%s\033[0m\n\n' "Copied app data to $rootbak_dir/Appdata." \
+        || err "Couldn't backup app data :\ "
 fi
 
 
 ### Commit ###
-printf "\n\033[1;34m%s\033[0m" "Do you want to commit the Backups dir to external NTFS ? (y/n): "
-read -n 1 isExporting
-printf "\n"
-
-if [[ $isExporting == "y" ]]; then
-    sudo bakcom.sh
+read -n 1 commit -p "Do you want to commit $rootbak_dir to external drive ? (y/n):"
+if [[ $commit == 'y' ]]; then
+    sudo bakcom.sh || die 'Error commiting backup.'
     paplay '/usr/share/sounds/freedesktop/stereo/complete.oga'
 
-    printf "\n\033[1;34m%s\033[0m" "Do you want to delete the Backups dir to save space on /home ? (y/n): "
-    read -n 1 isCleaning
-    printf "\n"
-
-    if [[ $isCleaning == "y" ]]; then
+    read -n 1 clean -p "Do you want to delete $rootbak_dir ? (y/n): "
+    if [[ $clean == 'y' ]]; then
+        # Preserve history backup
         mv "$rootbak_dir/hbak" "$TMPDIR/" && rm -fr "$rootbak_dir"
         mkdir "$rootbak_dir" && mv "$TMPDIR/hbak" "$rootbak_dir/"
-    else
-        echo "OK, big bakdir left as-is in home directory"
     fi
 
 else
-    err "Halted Commit."
+    err 'Halted Commit.'
 fi
 
-echo
-
-
-printf "Current dotfiles backup contents ...\n"
+printf 'Current dotfiles backups ...\n'
 [[ -d "$rootbak_dir/dots/" ]] && exa -alh --color=always --group-directories-first "$rootbak_dir/dots/"
